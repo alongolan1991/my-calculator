180b9efc1a9f1372927d81828c5ce3a81a48ee6c
alongolan1991 <33026025+alongolan1991@users.noreply.github.com>
2020-01-01 18:12:34 +0200

    Updates

---
+++ precache-manifest.28ded40c4cef23217ef6d0954032aafa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c98f90333b71dd67a40f33ac13d5ce",
+    "url": "/my-calculator/index.html"
+  },
+  {
+    "revision": "bb62e8a13bfd7ce48c6a",
+    "url": "/my-calculator/static/css/main.915dc083.chunk.css"
+  },
+  {
+    "revision": "01792565d6bb594da747",
+    "url": "/my-calculator/static/js/2.b5b1b548.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/my-calculator/static/js/2.b5b1b548.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb62e8a13bfd7ce48c6a",
+    "url": "/my-calculator/static/js/main.6c890f0e.chunk.js"
+  },
+  {
+    "revision": "c8d7aa08f7cf75bf8182",
+    "url": "/my-calculator/static/js/runtime-main.c55907ad.js"
+  },
+  {
+    "revision": "f5913285d144cccf69501048db48b633",
+    "url": "/my-calculator/static/media/backspace.f5913285.png"
+  }
+]);
